2e225fccfd3ff34cfbdbd548ec02242966772821
Guilbert Francois Antonio Alexis <guilbert@Guilberts-MacBook-Pro.local>
2023-11-17 10:25:09 +0400

    first preview

---
+++ main.js
@@ -0,0 +1,17 @@
+// owlCarousel 
+$('#slider-area').owlCarousel({
+    loop:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// owlCarousel end 
